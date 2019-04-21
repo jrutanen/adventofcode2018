ca5c4599169f928dbc915114a513bc42ccc27adb
Jani Rutanen <rautasissi@gmail.com>
2019-04-21 22:24:53 +0200

    day 6 part one and two solved

--- daysix/day6.go
+++ daysix/day6.go
@@ -50,12 +50,11 @@ func getDistance(from *coordinate, to *coordinate) int {
 
 func calculateArea(coords []*coordinate) {
 	xMin, xMax, yMin, yMax := findBorders(coords)
-	fmt.Printf("x: %d-%d y: %d-%d\n", xMin, xMax, yMin, yMax)
 	//area 0 means infinity
 	for ci, coord := range coords {
-		if coord.x == xMin || coord.x == xMax {
+		if coord.x <= xMin || coord.x >= xMax {
 			coord.area = 0
-		} else if coord.y == yMin || coord.y == yMax {
+		} else if coord.y <= yMin || coord.y >= yMax {
 			coord.area = 0
 		} else {
 			area := 0
@@ -74,7 +73,6 @@ func calculateArea(coords []*coordinate) {
 						}
 					}
 					if distanceToMe < distanceToOther {
-						//fmt.Printf("%d, %d : m: %d, o: %d\n", i, j, distanceToMe, distanceToOther)
 						area++
 					}
 				}
@@ -82,6 +80,63 @@ func calculateArea(coords []*coordinate) {
 			coord.area = area
 		}
 	}
+}
+
+func calculateAreaTwo(coords []*coordinate) {
+	xMin, xMax, yMin, yMax := findBorders(coords)
+	//area 0 means infinity
+	for ci, coord := range coords {
+		if coord.x <= xMin || coord.x >= xMax {
+			coord.area = 0
+		} else if coord.y <= yMin || coord.y >= yMax {
+			coord.area = 0
+		} else {
+			area := 0
+			for i := xMin - 100; i < xMax+100; i++ {
+				for j := yMin - 100; j < yMax+100; j++ {
+					currentPos := createCoordinate(i, j)
+					distanceToMe := getDistance(coord, currentPos)
+					distanceToOther := -1
+					for cj, coordJ := range coords {
+						//ignore self
+						if ci != cj {
+							distance := getDistance(coordJ, currentPos)
+							if distance < distanceToOther || distanceToOther == -1 {
+								distanceToOther = distance
+							}
+						}
+					}
+					if distanceToMe < distanceToOther {
+						area++
+					}
+				}
+			}
+			if coord.area < area {
+				coord.area = 0
+			}
+		}
+	}
+}
+
+func calculateDistances(coords []*coordinate, maxDistance int) int {
+	xMin, xMax, yMin, yMax := findBorders(coords)
+	//area 0 means infinity
+	var okLocations []coordinate
+
+	for i := xMin; i < xMax; i++ {
+		for j := yMin; j < yMax; j++ {
+			currentPos := createCoordinate(i, j)
+			totalDistance := 0
+			for _, coord := range coords {
+				distance := getDistance(coord, currentPos)
+				totalDistance = totalDistance + distance
+			}
+			if totalDistance < maxDistance {
+				okLocations = append(okLocations, *currentPos)
+			}
+		}
+	}
+	return len(okLocations)
 }
 
 func createCoordinate(x int, y int) *coordinate {
@@ -113,16 +168,24 @@ func main() {
 
 	//copy coordinates to struct
 	coordinates := createSliceOfCoordinates(input)
-	calculateArea(coordinates)
 
-	fmt.Printf("Number of coordinates: %d\n", len(coordinates))
+	//Part one
+	//What is the size of the largest area that isn't infinite?
+	calculateArea(coordinates)
+	//run through second time with bigger total area to see what areas are still growing (infinity)
+	calculateAreaTwo(coordinates)
+
 	//print the biggest area
 	maxArea := 0
 	for _, coord := range coordinates {
-		fmt.Printf("Coord area: %d\n", coord.area)
 		if coord.area > maxArea {
 			maxArea = coord.area
 		}
 	}
-	fmt.Printf("Max area: %d\n", maxArea)
+	fmt.Printf("Part One result: %d\n", maxArea)
+
+	//Part two
+	//What is the size of the region containing all locations which have a total
+	//distance to all given coordinates of less than 10000?
+	fmt.Printf("Part Two result: %d", calculateDistances(coordinates, 10000))
 }
--- daysix/day6_test.go
+++ daysix/day6_test.go
@@ -22,5 +22,20 @@ func TestArea(t *testing.T) {
 			t.Errorf("Wrong area. Expected %d got %d", expected[i], coord.area)
 		}
 	}
+}
 
+func TestDistances(t *testing.T) {
+	expected := 16
+	input, err := os.Open("input_day6_test.txt")
+	if err != nil {
+		fmt.Print(err)
+	}
+	defer input.Close()
+
+	coordinates := createSliceOfCoordinates(input)
+
+	area := calculateDistances(coordinates, 32)
+	if area != expected {
+		t.Errorf("Wrong area. Expected %d got %d", expected, area)
+	}
 }
